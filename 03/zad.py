aaeab9f4b936719ea8ef40361d9a802e880c0ea5
mjste <mj.stefanik@gmail.com>
2024-04-16 22:35:38 +0200

    finish half of tasks

--- 03/zad.py
+++ 03/zad.py
@@ -53,46 +53,34 @@ print(Vt)
 print("A")
 print(U @ S @ Vt)
 
-
-for n in range(2, 5):
-    for m in range(2, 5):
-        for k in range(1000):
-            A = np.random.rand(n, m)
-            U, S, Vt = SVD(A)
-            result = np.allclose(A, U @ S @ Vt)
-            if not result:
-                print("FAILED")
-                print(A)
-                print(U @ S @ Vt)
+print(np.linalg.svd(A))
 
 
-print("Matrix norm 1")
+# for n in range(2, 5):
+#     for m in range(2, 5):
+#         for k in range(1000):
+#             A = np.random.rand(n, m)
+#             U, S, Vt = SVD(A)
+#             result = np.allclose(A, U @ S @ Vt)
+#             if not result:
+#                 print("FAILED")
+#                 print(A)
+#                 print(U @ S @ Vt)
+
+
+print("Matrix norm 1 of matrix M")
 print(matrix_norm_1(M))
 print(np.linalg.norm(M, ord=1))
 print()
 
-print("Matrix norm inf")
+print("Matrix norm inf of matrix M")
 print(matrix_norm_inf(M))
 print(np.linalg.norm(M, ord=np.inf))
 print()
 
-print("Matrix norm 2")
+print("Matrix norm 2 of matrix M")
 print(matrix_norm_2(M))
 print(np.linalg.norm(M, ord=2))
 print()
 
 
-print("Matrix norm 1")
-print(matrix_norm_1(A))
-print(np.linalg.norm(A, ord=1))
-print()
-
-print("Matrix norm inf")
-print(matrix_norm_inf(A))
-print(np.linalg.norm(A, ord=np.inf))
-print()
-
-print("Matrix norm 2")
-print(matrix_norm_2(A))
-print(np.linalg.norm(A, ord=2))
-print()
